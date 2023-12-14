4eb3d600e6bd8bbe26e6405e71d006f0f301a52c
Nikita Protsanov <54556319+FalseDeckard@users.noreply.github.com>
2023-12-15 00:01:53 +0600

    Update main.py

--- main.py
+++ main.py
@@ -3,18 +3,22 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+# Load data
 url = "https://raw.githubusercontent.com/FalseDeckard/Streamlit_Homework/main/clients.csv"
 df = pd.read_csv(url, sep=";", index_col=0)
 
+# Data preprocessing
 df['GENDER'] = df['GENDER'].replace({0: 'Мужчина', 1: 'Женщина'})
 df['TARGET'] = df['TARGET'].replace({'Отклик не получен': 0, 'Отклик получен': 1})
 df['SOCSTATUS_WORK_FL'] = df['SOCSTATUS_WORK_FL'].replace({0: 'Не работает', 1: 'Работает'})
 df['SOCSTATUS_PENS_FL'] = df['SOCSTATUS_PENS_FL'].replace({0: 'Не пенсионер', 1: 'Пенсионер'})
 
+# Separate numerical features and data without target and ID
 df_num = df[['TARGET', 'AGE', 'CHILD_TOTAL', 'DEPENDANTS', 'PERSONAL_INCOME', 'LOAN_NUM_TOTAL', 'LOAN_NUM_CLOSED']]
 df_no_targ_id = df.drop(["TARGET", "AGREEMENT_RK"], axis=1)
 df_no_id = df.drop("AGREEMENT_RK", axis=1)
 
+# Russian translation dictionary
 rus = {'GENDER': 'ПОЛ', 'AGE': 'ВОЗРАСТ', 'CHILD_TOTAL': 'КОЛ-ВО ДЕТЕЙ',
        'DEPENDANTS': 'КОЛ-ВО ИЖДИВЕНЦЕВ', 'PERSONAL_INCOME': 'ПЕРСОНАЛЬНЫЙ ДОХОД',
        'LOAN_NUM_TOTAL': 'КОЛ-ВО КРЕДИТОВ', 'LOAN_NUM_CLOSED': 'КОЛ-ВО ЗАКРЫТЫХ КРЕДИТОВ',
@@ -27,6 +31,7 @@ def on_rus(feature):
     return f'{feature} - {rus[feature]}'
 
 
+# Function to plot count of target variable
 def count_target(target_col):
     st.subheader('Распределение целевой переменной')
     fig, ax = plt.subplots()
@@ -37,9 +42,10 @@ def count_target(target_col):
     st.write('Целевая переменная TARGET имеет дисбаланс в сторону отсутствия отклика (80%).')
 
 
+# Function to plot distribution of features
 def count_features(df):
     st.subheader('Распределение признаков')
-    feature = st.selectbox("Выберите признак:", df.columns, format_func=on_rus, key='5')
+    feature = st.sidebar.selectbox("Выберите признак:", df.columns, format_func=on_rus)
 
     if feature == 'GENDER' or feature == 'SOCSTATUS_WORK_FL' or feature == 'SOCSTATUS_PENS_FL':
         fig, ax = plt.subplots()
@@ -64,6 +70,7 @@ def count_features(df):
     ''')
 
 
+# Function to plot correlation matrix
 def mattrix(df):
     st.subheader('Матрица корреляции признаков')
     fig, ax = plt.subplots(figsize=(15, 15))
@@ -78,16 +85,18 @@ def mattrix(df):
     ''')
 
 
+# Function to display numerical characteristics of features
 def info(df):
     st.subheader('Числовые характеристики признаков')
-    feature = st.selectbox("Выберите признак:", df.columns, format_func=on_rus, key='2')
+    feature = st.sidebar.selectbox("Выберите признак:", df.columns, format_func=on_rus)
     st.write(df[feature].describe())
 
 
+# Function to plot pair-wise feature distributions
 def diagram_feature(df):
     st.subheader('Попарные распределения признаков')
-    feature_1 = st.selectbox("Выберите первый признак:", df.columns, format_func=on_rus, key='7')
-    feature_2 = st.selectbox("Выберите второй признак:", df.columns, format_func=on_rus, key='8')
+    feature_1 = st.sidebar.selectbox("Выберите первый признак:", df.columns, format_func=on_rus)
+    feature_2 = st.sidebar.selectbox("Выберите второй признак:", df.columns, format_func=on_rus)
 
     fig, ax = plt.subplots(figsize=(10, 5))
     sns.scatterplot(x=df[feature_1], y=df[feature_2], data=df, color='blue')
@@ -105,11 +114,10 @@ def diagram_feature(df):
     ''')
 
 
-
-
+# Function to plot target variable distribution with respect to features
 def diagram_with_target(df):
     st.subheader('Распределение целевой переменной в зависимости от признаков')
-    feature = st.selectbox("Выберите признак:", df.columns, format_func=on_rus, key='6')
+    feature = st.sidebar.selectbox("Выберите признак:", df.columns, format_func=on_rus)
 
     if feature == 'PERSONAL_INCOME' or feature == 'AGE':
         fig, ax = plt.subplots(figsize=(10, 5))
@@ -136,11 +144,13 @@ def diagram_with_target(df):
             - Чем меньше PERSONAL_INCOME, тем больше даты по таким клиентам;
             - Для AGE высокие показатели по отклику/отсутствию отклика приходятся с 22 лет до 40 лет - основная целевая аудитория.
     ''')
-       
+
+
+# Function to plot boxplots for selected features
 def boxplot_feature(df):
     st.subheader('Ящики с усами для выбранных признаков')
-    feature_1 = st.selectbox("Выберите первый признак:", df.columns, format_func=on_rus, key='10')
-    feature_2 = st.selectbox("Выберите второй признак:", df.columns, format_func=on_rus, key='11')
+    feature_1 = st.sidebar.selectbox("Выберите первый признак:", df.columns, format_func=on_rus)
+    feature_2 = st.sidebar.selectbox("Выберите второй признак:", df.columns, format_func=on_rus)
 
     fig, ax = plt.subplots(figsize=(10, 5))
     sns.boxplot(x=feature_1, y=feature_2, data=df)
@@ -150,10 +160,15 @@ def boxplot_feature(df):
     st.pyplot(fig)
     plt.close(fig)
 
+
 if __name__ == "__main__":
     st.title('EDA предобработанных данных клиентов банка')
     st.markdown("<br>", unsafe_allow_html=True)
 
+    st.sidebar.title('Выберите опции')
+    st.sidebar.subheader('Исследование данных')
+
+    st.markdown("<br><br>", unsafe_allow_html=True)
     st.subheader('Исследуем признаки и их взаимосвязь с целевой переменной, '
                  'числовые характеристики признаков, корреляцию признаков и т.д.')
     st.write('Исходные данные - база данных с информацией о клиентах банка и их персональных данных, '
@@ -171,7 +186,7 @@ if __name__ == "__main__":
     - LOAN_NUM_TOTAL — количество ссуд клиента;
     - LOAN_NUM_CLOSED — количество погашенных ссуд клиента.
                     ''')
-       
+
     st.markdown("<br><br>", unsafe_allow_html=True)
     count_features(df_no_targ_id)
 
@@ -179,8 +194,8 @@ if __name__ == "__main__":
     info(df_no_id)
 
     st.markdown("<br><br>", unsafe_allow_html=True)
-    count_target("TARGET")   
-       
+    count_target("TARGET")
+
     st.markdown("<br><br>", unsafe_allow_html=True)
     diagram_feature(df_no_targ_id)
 
@@ -189,6 +204,6 @@ if __name__ == "__main__":
 
     st.markdown("<br><br>", unsafe_allow_html=True)
     mattrix(df_num)
-       
-    #st.markdown("<br><br>", unsafe_allow_html=True)
-    #boxplot_feature(df)
+
+    st.markdown("<br><br>", unsafe_allow_html=True)
+    boxplot_feature(df)
