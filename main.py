d5ce36d02fc3d214a07ca5ffd66e08bd8a174a8b
Nikita Protsanov <54556319+FalseDeckard@users.noreply.github.com>
2023-12-14 23:25:46 +0600

    changed matplotlib to plotly.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
+import plotly as px
 
 url = "https://raw.githubusercontent.com/FalseDeckard/Streamlit_Homework/main/clients.csv"
 df = pd.read_csv(url, sep=";", index_col=0)
