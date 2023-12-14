85d27d2449dae6af95f40109b72ab187b8784e69
Nikita Protsanov <54556319+FalseDeckard@users.noreply.github.com>
2023-12-14 23:27:06 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import plotly as px
+import plotly.express as px
 
 url = "https://raw.githubusercontent.com/FalseDeckard/Streamlit_Homework/main/clients.csv"
 df = pd.read_csv(url, sep=";", index_col=0)
